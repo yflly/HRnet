3ac8ba01b3677d4507c3c163d0ab539d9a89f6fb
Yada Ly <lyyada@gmail.com>
2022-04-25 21:01:49 +0200

    add searchbar and css

--- src/pages/CurrentEmployee/CurrentEmployee.jsx
+++ src/pages/CurrentEmployee/CurrentEmployee.jsx
@@ -1,85 +1,47 @@
-import DataTable from "react-data-table-component";
-
-import { format } from "date-fns";
-import moment from "moment";
-
-import { useEffect, useState } from "react";
-//import { dataTableLabels } from "../../Data/data";
-
-const columns = [
-  {
-    name: "First Name",
-    selector: (row) => row.firstName,
-    sortable: true,
-  },
-  {
-    name: "Last Name",
-    selector: (row) => row.lastName,
-    sortable: true,
-  },
-  {
-    name: "Start Date",
-    selector: (row) => row.startDate,
-    sortable: true,
-  },
-  {
-    name: "Department",
-    selector: (row) => row.department,
-    sortable: true,
-  },
-  {
-    name: "Date of Birth",
-    selector: (row) => row.birthdate,
-    sortable: true,
-  },
-  {
-    name: "Street",
-    selector: (row) => row.street,
-    sortable: true,
-  },
-  {
-    name: "City",
-    selector: (row) => row.city,
-    sortable: true,
-  },
-  {
-    name: "State",
-    selector: (row) => row.state,
-    sortable: true,
-  },
-  {
-    name: "Zipcode",
-    selector: (row) => row.zipCode,
-    sortable: true,
-  },
-];
+import DataTables from "../../components/DataTable/DataTable";
+import data from "../../Data/DataMock.json";
+import "./CurrentEmployee.css";
+import { useState } from "react";
 
 export default function CurrentEmployee() {
-  const [employeesList, setEmployeesList] = useState([]);
-  /*const [key, setKey] = useState([]);*/
+  const [foundEmployees, setFoundEmployees] = useState("");
+  const localData = JSON.parse(localStorage.getItem("employees"));
+  let dataArr = [];
+  localData ? (dataArr = [...data, ...localData]) : (dataArr = [...data]);
 
-  useEffect(() => {
-    let employees = JSON.parse(localStorage.getItem("employees")) || [];
+  const handleSearch = (event) => {
+    console.log(dataArr);
+    const query = event.target.value;
+    console.log(query);
+    if (query.length >= 3) {
+      const results = dataArr.filter((employee) => {
+        return (
+          employee.lastName.toLowerCase().startsWith(query.toLowerCase()) ||
+          employee.firstName.toLowerCase().startsWith(query.toLowerCase()) ||
+          employee.department.toLowerCase().startsWith(query.toLowerCase())
+        );
+      });
+      console.log(results);
 
-    employees = employees.map((employee) => ({
-      ...employee,
-      birthdate: moment(employee.birthdate).format("MM/DD/yyyy"),
-      startDate: moment(employee.startDate).format("MM/DD/yyyy"),
-    }));
-
-    setEmployeesList(employees);
-    /*setKey(key + 1);*/
-  }, []);
+      setFoundEmployees(results);
+    } else {
+      setFoundEmployees(dataArr);
+    }
+  };
 
   return (
-    <>
-      <header className="header">
-        <div className="header-title">Current Employees</div>
-        <div className="header-separator" />
+    <main className="container">
+      <header className="container__header">
+        <h1 className="container__title">Current Employees</h1>
+        <input
+          type="search"
+          placeholder="Search..."
+          className="container__input"
+          onChange={handleSearch}
+        />
       </header>
-      <DataTable columns={columns} data={employeesList} />
-
-      <br />
-    </>
+      <p>{data.id}</p>
+      <DataTables data={foundEmployees || dataArr} />
+    </main>
   );
 }
