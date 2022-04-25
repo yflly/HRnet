705e8cbc42ace5fb9b68eed8437ea68d105b0ba1
Yada Ly <lyyada@gmail.com>
2022-04-25 18:05:11 +0200

    add title

--- src/pages/CreateEmployee/CreateEmployee.jsx
+++ src/pages/CreateEmployee/CreateEmployee.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect } from "react";
 import Form from "../../components/Form/Form";
+import "./CreateEmployee.css";
 
 export default function CreateEmployee() {
   useEffect(() => {
     document.title = "HRNET";
   });
   return (
-    <main>
+    <main className="wrapper">
+      <h1 className="container__title">Create Employee</h1>
       <Form />
     </main>
   );
