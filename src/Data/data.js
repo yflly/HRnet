117669b19b46f1052222476cc58531036139c870
Yada Ly <lyyada@gmail.com>
2022-04-25 21:02:36 +0200

    del dataTableLabels

--- src/Data/data.js
+++ src/Data/data.js
@@ -62,15 +62,3 @@ export const departments = [
   { value: "hr", label: "Human Resources" },
   { value: "legal", label: "Legal" },
 ];
-
-export const dataTableLabels = [
-  { text: "First Name", value: "firstName" },
-  { text: "Last Name", value: "lastName" },
-  { text: "Date of Birth", value: "birthdate" },
-  { text: "Start Date", value: "startDate" },
-  { text: "Street", value: "street" },
-  { text: "City", value: "city" },
-  { text: "State", value: "state" },
-  { text: "Zip Code", value: "zipCode" },
-  { text: "Department", value: "department" },
-];
